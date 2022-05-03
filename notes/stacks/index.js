fd81009f5e493864d344a3e90206396b6fd1d53e
Edgy-Toast <95452877+Edgy-Toast@users.noreply.github.com>
2022-05-02 21:16:08 -0500

    Adding the API

--- notes/stacks/index.js
+++ notes/stacks/index.js
@@ -1,12 +1,10 @@
-import MyStack from "./MyStack";
+import StorageStack from "./StorageStack";
+import ApiStack from "./ApiStack";
 
-export default function main(app) {
-  // Set default runtime for all functions
-  app.setDefaultFunctionProps({
-    runtime: "nodejs14.x"
-  });
+  export default function main(app) {
+    const storageStack = new StorageStack(app, "storage");
 
-  new MyStack(app, "my-stack");
-
-  // Add more stacks
-}
+      new ApiStack(app, "api", {
+        table: storageStack.table,
+    });
+  }
