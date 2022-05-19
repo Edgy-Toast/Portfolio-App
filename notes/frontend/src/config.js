42ac8c6c10f508c632059a72cfcd4a6e544dfd44
Edgy-Toast <95452877+Edgy-Toast@users.noreply.github.com>
2022-05-19 15:44:12 -0500

    Building my react app.

--- notes/frontend/src/config.js
+++ notes/frontend/src/config.js
@@ -1,4 +1,6 @@
 const config = {
+    STRIPE_KEY: "pk_test_51KvSOwCDpapd7PrlFXLlFbpYv1YOC5rz787uSrLldZem2AHZrItNEihNAh36UF4V9bcnb7rwxpsInFaliE6Tpabd00JjMhzmRU",
+    MAX_ATTACHMENT_SIZE: 5000000,
     // Backend config
     s3: {
       REGION: process.env.REACT_APP_REGION,
